24cbd0b537ca9f04a5dda3a2d25b0229fe6de46e
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-07-30 20:32:15 +0530

    js practice

---
+++ TimedCard/script.js
@@ -0,0 +1,17 @@
+let values = [5,3,22,44,3,5];
+let index = values.includes(5,2)
+
+console.log(index)
+
+let age = 10;
+let fname = "bob"
+
+console.log(fname+ " " + age)
+
+console.log(`${fname} is ${age} year old`)
+
+let fruits = ['apple','banna','orange']
+console.log(fruits.join(" "))
+
+let data = ["2024","7","29"]
+console.log(data.join("-"))
